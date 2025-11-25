f3b42460e397494c2c3a4f79618933992093fcda
HinsonBryson <bryson17hinson@gmail.com>
2025-11-25 07:54:05 -0500

    Refactor ProductBean class for improved readability

--- src/com/shashi/beans/ProductBean.java
+++ src/com/shashi/beans/ProductBean.java
@@ -6,83 +6,50 @@ import java.io.Serializable;
 @SuppressWarnings("serial")
 public class ProductBean implements Serializable {
 
-	public ProductBean() {
-	}
+    private String prodId;
+    private String prodName;
+    private String prodType;
+    private String prodInfo;
+    private double prodPrice;
+    private int prodQuantity;
+    private double pdiscount;   // product-only discount
+    private InputStream prodImage;
 
-	private String prodId;
-	private String prodName;
-	private String prodType;
-	private String prodInfo;
-	private double prodPrice;
-	private int prodQuantity;
-	private InputStream prodImage;
+    public ProductBean() {}
 
-	public ProductBean(String prodId, String prodName, String prodType, String prodInfo, double prodPrice,
-			int prodQuantity, InputStream prodImage) {
-		super();
-		this.prodId = prodId;
-		this.prodName = prodName;
-		this.prodType = prodType;
-		this.prodInfo = prodInfo;
-		this.prodPrice = prodPrice;
-		this.prodQuantity = prodQuantity;
-		this.prodImage = prodImage;
-	}
+    public ProductBean(String prodId, String prodName, String prodType, String prodInfo,
+                       double prodPrice, int prodQuantity, double pdiscount, InputStream prodImage) {
+        this.prodId = prodId;
+        this.prodName = prodName;
+        this.prodType = prodType;
+        this.prodInfo = prodInfo;
+        this.prodPrice = prodPrice;
+        this.prodQuantity = prodQuantity;
+        this.pdiscount = pdiscount;
+        this.prodImage = prodImage;
+    }
 
-	public String getProdId() {
-		return prodId;
-	}
+    public String getProdId() { return prodId; }
+    public void setProdId(String prodId) { this.prodId = prodId; }
 
-	public void setProdId(String prodId) {
-		this.prodId = prodId;
-	}
+    public String getProdName() { return prodName; }
+    public void setProdName(String prodName) { this.prodName = prodName; }
 
-	public String getProdName() {
-		return prodName;
-	}
+    public String getProdType() { return prodType; }
+    public void setProdType(String prodType) { this.prodType = prodType; }
 
-	public void setProdName(String prodName) {
-		this.prodName = prodName;
-	}
+    public String getProdInfo() { return prodInfo; }
+    public void setProdInfo(String prodInfo) { this.prodInfo = prodInfo; }
 
-	public String getProdType() {
-		return prodType;
-	}
+    public double getProdPrice() { return prodPrice; }
+    public void setProdPrice(double prodPrice) { this.prodPrice = prodPrice; }
 
-	public void setProdType(String prodType) {
-		this.prodType = prodType;
-	}
+    public int getProdQuantity() { return prodQuantity; }
+    public void setProdQuantity(int prodQuantity) { this.prodQuantity = prodQuantity; }
 
-	public String getProdInfo() {
-		return prodInfo;
-	}
+    public double getPdiscount() { return pdiscount; }
+    public void setPdiscount(double pdiscount) { this.pdiscount = pdiscount; }
 
-	public void setProdInfo(String prodInfo) {
-		this.prodInfo = prodInfo;
-	}
-
-	public double getProdPrice() {
-		return prodPrice;
-	}
-
-	public void setProdPrice(double prodPrice) {
-		this.prodPrice = prodPrice;
-	}
-
-	public int getProdQuantity() {
-		return prodQuantity;
-	}
-
-	public void setProdQuantity(int prodQuantity) {
-		this.prodQuantity = prodQuantity;
-	}
-
-	public InputStream getProdImage() {
-		return prodImage;
-	}
-
-	public void setProdImage(InputStream prodImage) {
-		this.prodImage = prodImage;
-	}
-
+    public InputStream getProdImage() { return prodImage; }
+    public void setProdImage(InputStream prodImage) { this.prodImage = prodImage; }
 }
