91b0702dfd30f526143fa00bce5e137532f61267
HinsonBryson <bryson17hinson@gmail.com>
2025-11-25 07:55:02 -0500

    Refactor ProductService interface methods

--- src/com/shashi/service/ProductService.java
+++ src/com/shashi/service/ProductService.java
@@ -7,32 +7,47 @@ import com.shashi.beans.ProductBean;
 
 public interface ProductService {
 
-	public String addProduct(String prodName, String prodType, String prodInfo, double prodPrice, int prodQuantity,
-			InputStream prodImage);
+    // Add product using fields
+    public String addProduct(String prodName, String prodType, String prodInfo,
+                             double prodPrice, int prodQuantity, double pdiscount,
+                             InputStream prodImage);
 
-	public String addProduct(ProductBean product);
+    // Add product using ProductBean
+    public String addProduct(ProductBean product);
 
-	public String removeProduct(String prodId);
+    // Remove a product
+    public String removeProduct(String prodId);
 
-	public String updateProduct(ProductBean prevProduct, ProductBean updatedProduct);
+    // Update product (with image)
+    public String updateProduct(ProductBean prevProduct, ProductBean updatedProduct);
 
-	public String updateProductPrice(String prodId, double updatedPrice);
+    // Update product (without image)
+    public String updateProductWithoutImage(String prevProductId, ProductBean updatedProduct);
 
-	public List<ProductBean> getAllProducts();
+    // Update only price
+    public String updateProductPrice(String prodId, double updatedPrice);
 
-	public List<ProductBean> getAllProductsByType(String type);
+    // Fetch all products
+    public List<ProductBean> getAllProducts();
 
-	public List<ProductBean> searchAllProducts(String search);
+    // Fetch all products by type
+    public List<ProductBean> getAllProductsByType(String type);
 
-	public byte[] getImage(String prodId);
+    // Search products by keyword
+    public List<ProductBean> searchAllProducts(String search);
 
-	public ProductBean getProductDetails(String prodId);
+    // Get product image
+    public byte[] getImage(String prodId);
 
-	public String updateProductWithoutImage(String prevProductId, ProductBean updatedProduct);
+    // Get full product details
+    public ProductBean getProductDetails(String prodId);
 
-	public double getProductPrice(String prodId);
+    // Sell N quantity of a product
+    public boolean sellNProduct(String prodId, int n);
 
-	public boolean sellNProduct(String prodId, int n);
+    // Get price of a product
+    public double getProductPrice(String prodId);
 
-	public int getProductQuantity(String prodId);
+    // Get quantity of a product
+    public int getProductQuantity(String prodId);
 }
